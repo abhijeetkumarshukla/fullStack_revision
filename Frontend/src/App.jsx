49821eb4aa1fe0dc93e8d720ab651a156322d9aa
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2025-01-07 23:30:32 +0530

    signup done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+// import Login from './pages/login/Login'
+import Signup from './pages/register/Signup'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="text-center">
+    <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
+      {/* <Login/> */}
+      <Signup/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/register/Signup.jsx
@@ -0,0 +1,24 @@
+
+
+const Signup = () => {
+  return (
+    <div>
+     <form   >
+        <label >UserName</label>
+        <input type="text" placeholder="username"  />
+
+        <label >Email</label>
+        <input type="email" placeholder="Email"  />
+
+        <label >Password</label>
+        <input type="password" placeholder="password"  />
+
+        <button className="">signup</button>
+
+
+     </form>
+    </div>
+  )
+}
+
+export default Signup
